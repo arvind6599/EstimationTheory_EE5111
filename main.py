88da34eaaa648639167ac480f54805d7899512ee
2vrk1504 <vallabh.developer@gmail.com>
2020-02-04 18:43:17 +0530

    Q3 done

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+TOTAL_ITERATIONS = 100
+
 # Parameters
+
 N = 512		# dimensions of signal detected
 L = 32 		# channel taps
 
 k0 = 6		# sparsity of h vector
+gb = 180	# guard bands on either side
 
-n_sigma = 0.1			# noise during estimation
+n_sigma_simulation = [0.1**0.5, 0.01**0.5]	# noise during estimation
+
 c_sigma = 0.5 ** 0.5 	# channel parameter
 lambda_ = 0.2			# channel parameter
 
-QPSK_SYMBOLS = [1+1j, -1+1j, 1-1j, -1-1j]
+alpha = 0.1		# regularization
 
-input_data = np.random.randint(len(QPSK_SYMBOLS), size=N) # to generate the bits
-
-# generate X matrix
-X = np.diag([QPSK_SYMBOLS[sym] for sym in input_data])
-
-# print('Input data:')
-# print(''.join(['00' if v==0 else '01' if v==1 else '10' if v==2 else '11' for v in input_data]))
+def print_channel(h, text):
+	print(text)
+	for i, hk in enumerate(h):
+		print(i+1, ' : ', hk)
+	print()
 
 p = np.exp(-lambda_ * (np.arange(1,L+1) - 1)).reshape((L,1))
 a = np.random.normal(0, c_sigma, (L,1))
@@ -28,78 +31,108 @@ norm_p = np.sum(p**2)
 
 # h0 is the true channel impulse response vector
 h0 = ((a + 1j*b)*p)/norm_p	# h[k] = (a[k] + jb[k])p[k] / norm(p)
-print('h_actual')
-for i, hk in enumerate(h0):
-	print(i+1, ' : ', hk)
-print()
 
 # building the F matrix
 F = np.array([ [ np.exp((np.pi*2j*i*j)/N) for j in range(L)] for i in range(N)])
 
-XF = X.dot(F)
-y = XF.dot(h0) + np.random.normal(0, n_sigma, (N,1)) # process noise
-XFH = XF.conjugate().T
-XFH_XF_inv = np.linalg.inv(XFH.dot(XF))
+def plot_channel(hplot, label):
+	plt.figure()
+	plt.subplot(2,1,1)
+	plt.title(label + ' Magnitude and Phase')
+	plt.grid(True)
+	plt.stem([abs(hh[0]) for hh in h0], linefmt='g-', markerfmt='go', label='Original', use_line_collection=True)
+	plt.stem([abs(hh[0]) for hh in hplot], linefmt='r:', markerfmt='ro', label=label, use_line_collection=True)
+	plt.legend(loc='upper right')
+	plt.subplot(2,1,2)
+	plt.grid(True)
+	plt.stem([np.angle(hh[0]) for hh in h0], linefmt='g-', markerfmt='go', label='Original', use_line_collection=True)
+	plt.stem([np.angle(hh[0])for hh in hplot], linefmt='r:', markerfmt='ro', label=label, use_line_collection=True)
+	plt.legend(loc='upper right')
 
-######################## QUESTION - 1 Least Squares Estimate ####################################
-h1 = XFH_XF_inv.dot(XFH).dot(y)		# estimate 1
-print('Q1. h_est')
-for i, hk in enumerate(h1):
-	print(i+1, ' : ', hk)
-print()
+QPSK_SYMBOLS = [1+1j, -1+1j, 1-1j, -1-1j]
 
-################### QUESTION - 2 Least Squares Estimate with sparsity ###########################
-# A(h_est) = b; b = [0 0 ... 0]^T
-# selected entries are zero
-A = np.zeros((L-k0, L))
-zero_indices = np.arange(26) #np.random.choice(L, L-k0, replace=False)
-for i, j in enumerate(zero_indices):
-	A[i][j] = 1 
+# Expectation values:
+Eh1 = np.zeros((L,1), dtype=np.complex128)
+Eh2 = np.zeros((L,1), dtype=np.complex128)
+Eh3 = np.zeros((L,1), dtype=np.complex128)
+Eh4 = np.zeros((L,1), dtype=np.complex128)
 
-# Least squares with constraints
-XFH_XF_inv_AT = XFH_XF_inv.dot(A.T)
-A_XFH_XF_inv_AT_inv = np.linalg.inv(A.dot(XFH_XF_inv_AT))
-h2 = h1 - XFH_XF_inv_AT.dot(A_XFH_XF_inv_AT_inv).dot(A).dot(h1)
 
-print('Q2. h_est')
-for i, hk in enumerate(h2):
-	print(i+1, ' : ', hk)
-print()
+for n_sigma in n_sigma_simulation:
+	print('process noise sigma: ', n_sigma)
+	for iteration in range(TOTAL_ITERATIONS):
+		process_noise = np.random.normal(0, n_sigma, (N,1)) # process noise
+		input_data = np.random.randint(len(QPSK_SYMBOLS), size=N) # to generate the bits
 
-################### QUESTION - 4 Least Squares Estimate with constraints ##########################
-# A(h_est) = b; b = [0 0 0]^T
-# selected entries are zero
-A = np.zeros((3, L))
-A[0][0] = 1; A[0][1] = -1; # h[1] = h[2]
-A[1][2] = 1; A[1][3] = -1; # h[3] = h[4]
-A[2][4] = 1; A[2][5] = -1; # h[5] = h[6]
+		# generate X matrix
+		X = np.diag([QPSK_SYMBOLS[sym] for sym in input_data])
 
-# Least squares with constraints
-XFH_XF_inv_AT = XFH_XF_inv.dot(A.T)
-A_XFH_XF_inv_AT_inv = np.linalg.inv(A.dot(XFH_XF_inv_AT))
-h4 = h1 - XFH_XF_inv_AT.dot(A_XFH_XF_inv_AT_inv).dot(A).dot(h1)
+		XF = X.dot(F)
+		y = XF.dot(h0) + process_noise
+		XFH = XF.conjugate().T
+		XFH_XF_inv = np.linalg.inv(XFH.dot(XF))
 
-print('Q4. h_est')
-for i, hk in enumerate(h4):
-	print(i+1, ' : ', hk)
-print()
+		######################## QUESTION - 1 Least Squares Estimate ####################################
+		h1 = XFH_XF_inv.dot(XFH).dot(y)		# estimate 1
+		Eh1 = (iteration)/(iteration + 1) * Eh1 + h1/(iteration + 1)
 
-plt.figure()
-plt.title('Real')
-plt.grid(True)
-plt.plot([hh[0].real for hh in h0], label='Original')
-plt.plot([hh[0].real for hh in h1], label='Q1')
-plt.plot([hh[0].real for hh in h2], label='Q2')
-plt.plot([hh[0].real for hh in h4], label='Q4')
-plt.legend(loc='upper right')
+		################### QUESTION - 2 Least Squares Estimate with sparsity ###########################
+		non_zero_indices = np.arange(L-k0, L)
+		h2 = np.zeros((L,1), dtype=np.complex128); 
+		for ii in non_zero_indices:
+			h2[ii] = h1[ii]
+		Eh2 = (iteration)/(iteration + 1) * Eh2 + h2/(iteration + 1)
 
-plt.figure()
-plt.title('Imaginary')
-plt.grid(True)
-plt.plot([hh[0].imag for hh in h0], label='Original')
-plt.plot([hh[0].imag for hh in h1], label='Q1')
-plt.plot([hh[0].imag for hh in h2], label='Q2')
-plt.plot([hh[0].imag for hh in h4], label='Q4')
-plt.legend(loc='upper right')
+		######################## QUESTION - 3 Least Squares Estimate Guard Band ##############################
+		X1 = X[gb:-gb, gb:-gb]
+		X1_F = X1.dot(F[gb:-gb,:])
+		y1 = X1_F.dot(h0) + process_noise[gb:-gb]
 
-plt.show()
+		X1_FH = X1_F.conjugate().T
+		X1_FH_X1_F_inv = np.linalg.inv(X1_FH.dot(X1_F))
+		# h3_1a = X1_FH_X1_F_inv.dot(X1_FH).dot(y1)
+
+		# h3_2a = np.zeros((L,1)); 
+		# for ii in non_zero_indices:
+		# 	h3_2a[ii] = h3_1a[ii]
+
+		# print_channel(h3_1a, 'Q3. (1a) h_est')
+		# print_channel(h3_2a, 'Q3. (2a) h_est')
+
+		# Applying regularisation
+		X1_FH_X1_F_inv_reg = np.linalg.inv(X1_FH.dot(X1_F) + alpha * np.eye(L))
+		h3_b = X1_FH_X1_F_inv_reg.dot(X1_FH).dot(y1)
+		Eh3 = (iteration)/(iteration + 1) * Eh3 + h3_b/(iteration + 1)
+		
+
+
+		################### QUESTION - 4 Least Squares Estimate with constraints ##########################
+		# A(h_est) = b; b = [0 0 0]^T
+		# selected entries are zero
+		A = np.zeros((3, L), dtype=np.complex128)
+		A[0][0] = 1; A[0][1] = -1; # h[1] = h[2]
+		A[1][2] = 1; A[1][3] = -1; # h[3] = h[4]
+		A[2][4] = 1; A[2][5] = -1; # h[5] = h[6]
+
+		# Least squares with constraints
+		XFH_XF_inv_AT = XFH_XF_inv.dot(A.T)
+		A_XFH_XF_inv_AT_inv = np.linalg.inv(A.dot(XFH_XF_inv_AT))
+		h4 = h1 - XFH_XF_inv_AT.dot(A_XFH_XF_inv_AT_inv).dot(A).dot(h1)
+		Eh4 = (iteration)/(iteration + 1) * Eh4 + h4/(iteration + 1)
+		
+
+		if iteration == TOTAL_ITERATIONS - 1:
+			print('MSE(h1) = ', np.sum(abs(Eh1-h0)**2)/L)
+			print_channel(Eh1, 'Eh1')
+			print('MSE(h2) = ', np.sum(abs(Eh2-h0)**2)/L)
+			print_channel(Eh2, 'Eh2')
+			print('MSE(h3) = ', np.sum(abs(Eh3-h0)**2)/L)
+			print_channel(Eh3, 'Eh3')
+			print('MSE(h4) = ', np.sum(abs(Eh4-h0)**2)/L)
+			print_channel(Eh4, 'Eh4')
+			plot_channel(h1, 'h1')
+			plot_channel(h2, 'h2')
+			plot_channel(h3_b, 'h3_b')
+			plot_channel(h4, 'h4')
+			print('//////////////////////////////////////////////////////////////////')
+			plt.show()
