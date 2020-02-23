76a74ce9d63c236e66965fb93c9cf39a3acb1c92
sadidaa <adityasundar99@gmail.com>
2020-02-23 20:57:46 +0530

    Updated assignment 2 code

--- Sadi_Assignment2/Assignment2.py
+++ Sadi_Assignment2/Assignment2.py
@@ -1,10 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 	
+def newton_rhaphson(x,thresh,gamma):	
+	theta_0 = np.median(x,0) 
+	b = (x-theta_0)/gamma
+	l_1 = 2*np.sum(b /(1 + b*b),0)
+	while(np.maximum(np.abs(l_1)) > thresh):
+		l_2 = 2*sum((b*b - 1)/((1 + b*b)*(1 + b*b)))
+		theta_0 = theta_0 - l_1/l_2
+		b = (x - theta_0)/gamma
+		l_1 = 2*sum(b /(1 + b*b))
+	return theta_0
+
 def main():
 	Q_no = 1 # Question number in assignment	
 	N = 1000 # Number of samples
 	A = 1 	# DC Value to be estimated
+	thresh = 0.0001
 			
 	iters = 100000 # Number of iterations run for finding the mean and variance of the estimator 
 
@@ -18,9 +30,10 @@ def main():
 		X = A + noise
 		A_estimated = np.median(X,0)  # shape = (iters,1) and contains the MLE estimated value of A for each iteration
 	else:
-		noise = np.random.standard_cauchy((N,iters))*((2*1.78)**0.5)
+		gamma = ((2*1.78)**0.5)
+		noise = np.random.standard_cauchy((N,iters))*((2*1.78)**0.5)*gamma
 		X = A + noise
-		A_estimated = np.median(X,0)  # shape = (iters,1) and contains the MLE estimated value of A for each iteration
+		A_estimated = newton_rhaphson(X,thresh,gamma)  # shape = (iters,1) and contains the MLE estimated value of A for each iteration
 
 	# Finding mean and variance of estimator
 	E_A_estimated = np.mean(A_estimated)
