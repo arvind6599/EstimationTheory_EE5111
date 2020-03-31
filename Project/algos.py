401a966f7e55a1de9f9092270e9286c024be6396
2vrk1504 <vallabh.developer@gmail.com>
2020-03-31 12:44:52 +0530

    Corrected initialization and refactored

---
+++ Project/algos.py
@@ -0,0 +1,76 @@
+import numpy as np 
+
+def P_gaussian(x, mu, sigma, beta):
+		return ((1/(2*np.pi*sigma**2)**0.5)*np.exp(-((x-mu)**2)/2/sigma**2))**beta
+
+def likelihood(alpha, x, mu, sigma, beta):
+	return ((alpha**beta)*P_gaussian(x, mu[0], sigma[0], beta) + ((1-alpha)**beta)*P_gaussian(x, mu[1], sigma[1], beta))
+
+def ds_error(alpha, mu, sigma, alpha_est, mu_est, sigma_est):
+	# Symmetric KL divergence
+	if abs(1 - alpha - alpha_est) < abs(alpha - alpha_est):
+		mu_est = [mu_est[1], mu_est[0]]
+		sigma_est = [sigma_est[1], sigma_est[0]]
+	err = 0
+	for i in range(len(mu)):
+		err += 0.5*((sigma[i]/sigma_est[i])**2 + (sigma_est[i]/sigma[i])**2)
+		err += 0.5*((mu_est[i] - mu[i])*(1/sigma[i]**2 + 1/sigma_est[i]**2)*(mu_est[i] - mu[i]))
+		err -= 1 # dimension
+	return err
+
+class Solver:
+
+	def __init__(self, mu, sigma, alpha):
+		self.mu = mu
+		self.sigma = sigma
+		self.alpha = alpha
+	
+	def DAEM_GMM(self, X, thresh, mu_est=None, sigma_est=None, alpha_est=None, betas=[0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.0], K=2):
+		"""
+			Deterministic Annealing EM Algorithm for k=2 Gaussians
+		"""
+		N = X.size
+		errors = []
+		steps = 0
+		# Initial estimates
+		if alpha_est is None:
+			alpha_est = 1./K
+		if mu_est is None:
+			mu_est = [X[int(np.random.random()*N)] for j in range(K)]
+		if sigma_est is None:
+			sample_mean = np.sum(X)/N
+			cov = np.sum((X-sample_mean)**2)/N
+			sigma_est = [cov, cov]
+
+		for beta in betas:
+			llh_01 = likelihood(alpha_est, X, mu_est, sigma_est, 1)
+			llh_1 = likelihood(alpha_est, X, mu_est, sigma_est, beta)
+			h = (alpha_est**beta)*P_gaussian(X, mu_est[0], sigma_est[0], beta)/llh_1
+			tolerance = np.array([1, 1, 1, 1, 1])
+			while np.any(tolerance >= thresh) and steps <= 100000:
+				steps += 1
+				# print("Step {}".format(steps))
+				llh_00 = llh_01
+				llh_0 = llh_1
+
+				h_tot = np.sum(h)
+				mu_est[0] = np.sum(h*X)/h_tot
+				mu_est[1] = np.sum((1-h)*X)/(N-h_tot)
+				sigma_est[0] = (np.sum(h*(X-mu_est[0])**2)/h_tot)**0.5
+				sigma_est[1] = (np.sum((1-h)*(X-mu_est[1])**2)/(N - h_tot))**0.5
+				alpha_est = h_tot/N
+
+				llh_1 = likelihood(alpha_est, X, mu_est, sigma_est, beta)
+				llh_01 = likelihood(alpha_est, X, mu_est, sigma_est, 1)
+
+				h = (alpha_est**beta)*P_gaussian(X, mu_est[0], sigma_est[0], beta)/llh_1
+				tolerance = np.abs((llh_01-llh_00)/llh_00)
+				errors.append(ds_error(self.alpha, self.mu, self.sigma, alpha_est, mu_est, sigma_est))
+
+		return alpha_est, mu_est, sigma_est, errors, steps
+
+	def EM_GMM(self, X, thresh, mu_est=None, sigma_est=None, alpha_est=None):
+		"""
+			Regular Expectation Maximization
+		"""
+		return self.DAEM_GMM(X=X, thresh=thresh, mu_est=mu_est, sigma_est=sigma_est, alpha_est=alpha_est, betas=[1])
--- Project/em_gmm.py
+++ Project/em_gmm.py
@@ -1,97 +1,56 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+from algos import Solver
 
-thresh = 1e-6
-
-N = 1000
-mus = [3]		# means of the mixed Gaussians
-alpha = 0.5				# mixing coefficient
+N = 1000		# number of samples
+K = 2			# number of mixed Gaussians
+mus = [10]		# means of the mixed Gaussians
 sigma = [2.5, 2.5]		# std deviation
 
-# X = np.append(np.random.normal(mu[0], sigma[0], int(N*alpha)), np.random.normal(mu[1], sigma[1], N-int(N*alpha))) 
-# np.random.shuffle(X) # random samples drawn from the GMM
-
-
-def P_gaussian(x, mu, sigma, beta):
-	return ((1/(2*np.pi*sigma**2)**0.5)*np.exp(-((x-mu)**2)/2/sigma**2))**beta
-
-def likelihood(alpha, x, mu, sigma, beta):
-	return ((alpha_est**beta)*P_gaussian(x, mu[0], sigma[0], beta) + ((1-alpha_est)**beta)*P_gaussian(x, mu[1], sigma[1], beta))
-
-def ds_error(alpha, mu, sigma, alpha_est, mu_est, sigma_est):
-	# Symmetric KL divergence
-	if abs(1 - alpha - alpha_est) < abs(alpha - alpha_est):
-		mu_est = [mu_est[1], mu_est[0]]
-		sigma_est = [sigma_est[1], sigma_est[0]]
-	err = 0
-	for i in range(len(mu)):
-		err += 0.5*((sigma[i]/sigma_est[i])**2 + (sigma_est[i]/sigma[i])**2)
-		err += 0.5*((mu_est[i] - mu[i])*(1/sigma[i]**2 + 1/sigma_est[i]**2)*(mu_est[i] - mu[i]))
-		err -= 1 # dimension
-	return err
+def toss(alpha):
+	x = np.random.random()
+	if(x<=alpha):
+		return 0
+	else:
+		return 1
 
 
 alphas = np.linspace(0.1, 0.6, 3) # mixing coefficients
-
-errors = []
 for mu_iter in mus:
-	print(mu_iter)
 	mu = [-mu_iter, mu_iter]
-	errorss = []
+	errorss_daem = []
+	errorss_em = []
+
 	for alpha in alphas:
-		X = np.append(np.random.normal(mu[0], sigma[0], int(N*alpha)), np.random.normal(mu[1], sigma[1], N-int(N*alpha))) 
-		np.random.shuffle(X) # random samples drawn from the GMM
+		solver = Solver(alpha=alpha, mu=mu, sigma=sigma)
 
-		# print('alpha: {}, mu: ({}, {}), sigma: ({}, {})'.format(alpha, mu[0], mu[1], sigma[0], sigma[1]))
-		# plt.figure('True PDF')
-		# n_bins = 30
-		# bins = np.linspace(min(mu)-3*max(sigma), max(mu)+3*max(sigma), n_bins)
-		# vals, bins_pos,patch = plt.hist(X, bins, density=True)
-		# plt.plot(bins_pos[:-1]+(bins[1]-bins[0])/2, vals, label="N="+str(N))
-		# plt.grid(True)
-		# plt.show()
+		X = np.array([np.random.normal(mu[toss(alpha)], sigma[toss(alpha)]) for j in range(N)])
+		print('Actual:')
+		print('alpha: {}, mu: {}, sigma: {}'.format(alpha, mu, sigma))
+		alpha_est_daem, mu_est_daem, sigma_est_daem, errors_daem, steps = solver.DAEM_GMM(X=X, thresh=1e-10)
+		errorss_daem.append(errors_daem)
+		print('DAEM')
+		print('alpha_est: {}, mu_est: {}, sigma_est: {}'.format(alpha_est_daem, mu_est_daem, sigma_est_daem))
+		alpha_est_em, mu_est_em, sigma_est_em, errors_em, steps = solver.EM_GMM(X=X, thresh=1e-10)
+		errorss_em.append(errors_em)
+		print('EM')
+		print('alpha_est: {}, mu_est: {}, sigma_est: {}'.format(alpha_est_em, mu_est_em, sigma_est_em))
+		print()
 
-		errors = []
-		steps = 0
-		alpha_est = 0.4
-		betas = [0.75, 1.0, 1.2, 1.0]
-		# EM algorithm
-		# Initial estimates
-		mu_est = [0, 1]
-		sigma_est = [1., 1.]
-
-		for beta in betas:
-			llh_01 = likelihood(alpha_est, X, mu_est, sigma_est, 1)
-			llh_1 = likelihood(alpha_est, X, mu_est, sigma_est, beta)
-			h = (alpha_est**beta)*P_gaussian(X, mu_est[0], sigma_est[0], beta)/llh_1
-			tolerance = np.array([1, 1, 1, 1, 1])
-			while np.any(tolerance >= thresh) and steps <= 100000:
-				steps += 1
-				# print("Step {}".format(steps))
-				llh_00 = llh_01
-				llh_0 = llh_1
-
-				h_tot = np.sum(h)
-				mu_est[0] = np.sum(h*X)/h_tot
-				mu_est[1] = np.sum((1-h)*X)/(N-h_tot)
-				sigma_est[0] = (np.sum(h*(X-mu_est[0])**2)/h_tot)**0.5
-				sigma_est[1] = (np.sum((1-h)*(X-mu_est[1])**2)/(N - h_tot))**0.5
-				alpha_est = h_tot/N
-
-				llh_1 = likelihood(alpha_est, X, mu_est, sigma_est, beta)
-				llh_01 = likelihood(alpha_est, X, mu_est, sigma_est, 1)
-
-				h = (alpha_est**beta)*P_gaussian(X, mu_est[0], sigma_est[0], beta)/llh_1
-				tolerance = np.abs((llh_01-llh_00)/llh_00)
-				errors.append(ds_error(alpha, mu, sigma, alpha_est, mu_est, sigma_est))
-		errorss.append(errors)
-		print('alpha_est: {}, mu_est: ({}, {}), sigma_est: ({}, {})'.format(alpha_est, mu_est[0], mu_est[1], sigma_est[0], sigma_est[1]))
-	plt.figure()
-	plt.title(r'Error vs. Iterations, $(\mu_1,\mu_2)=($'+str(-mu_iter)+','+str(mu_iter)+')')
+	plt.figure('DAEM')
+	plt.title(r'DAEM Error vs. Iterations, $(\mu_1,\mu_2)=($'+str(-mu_iter)+','+str(mu_iter)+')')
 	for i, alpha in enumerate(alphas):
-		plt.plot(errorss[i], 'x-', label=r'$\alpha=$'+str(alpha))
+		plt.plot(errorss_daem[i], 'x-', label=r'$\alpha=$'+str(alpha))
 	plt.grid(True)
 	plt.legend(loc='upper right')
+
+	plt.figure('EM')
+	plt.title(r'EM Error vs. Iterations, $(\mu_1,\mu_2)=($'+str(-mu_iter)+','+str(mu_iter)+')')
+	for i, alpha in enumerate(alphas):
+		plt.plot(errorss_em[i], 'x-', label=r'$\alpha=$'+str(alpha))
+	plt.grid(True)
+	plt.legend(loc='upper right')
+
 	plt.show()
 
 		# stepss.append(steps);
@@ -106,6 +65,3 @@ for mu_iter in mus:
 # plt.grid(True)
 # plt.legend(loc='upper right')
 # plt.show()
-
-print('alpha_est: {}, mu_est: ({}, {}), sigma_est: ({}, {})'.format(alpha_est, mu_est[0], mu_est[1], sigma_est[0], sigma_est[1]))
-print('Steps taken: {}'.format(steps))
